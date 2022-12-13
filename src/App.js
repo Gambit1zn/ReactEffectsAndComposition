bd41ca08f3a2ae41ef3e9ae1a739c5cf558da899
Ethan Ankadu <ethan.ankadu@gamesglobal.com>
2022-12-13 20:53:00 +0200

    added the use of context api using both consumer and useConsumer react hook

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import React, { useState, useEffect } from 'react';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
 
   useEffect(() => {
-    if (storedUserLoggedInInformation === '1')
-    {
+    if (storedUserLoggedInInformation === '1') {
       setIsLoggedIn(true);
     }
     // eslint-disable-next-line
@@ -24,18 +24,22 @@ function App() {
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem('isLoggedIn','1');
+    localStorage.removeItem('isLoggedIn', '1');
     setIsLoggedIn(false);
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn 
+      }}
+    >
+      <MainHeader onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
   );
 }
 
